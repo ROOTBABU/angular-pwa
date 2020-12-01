030208000c983a3249f41f1df46ff72a95fe2155
ROOTBABU <vikas25019@gmail.com>
2020-12-01 19:04:00 +0530

    add swUpdate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component,OnInit } from '@angular/core';
 import {SwUpdate} from '@angular/service-worker/';
 import { UserService } from 'src/app/service/user.service'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,18 +10,41 @@ import { UserService } from 'src/app/service/user.service'
 
 export class AppComponent implements OnInit {
   userData;
-  constructor(private userService:UserService){ 
+  constructor(private userService:UserService,private update:SwUpdate){ 
   }
   ngOnInit(): void {
     this.getUserData();
+    this.updateUser();
   }
   getUserData(){
     this.userService.getUserDetails().subscribe(res=>{
-      console.log(`${res.data}`);
       this.userData = res.data;
     },err=>{
       console.log(`error`);
     })
   }
 
+  updateUser(){
+    if(!this.update.isEnabled){
+      console.log(`not Enabled`);
+      return;
+    }
+    this.update.available.subscribe((event)=>{
+      console.log(`event:`,event.current);
+      console.log(`event:`,event.available);
+      if(confirm('update availbable')){
+        this.update.activateUpdate().then(()=>{
+          location.reload();
+        })
+      }
+    },err=>{
+      console.log(`${err}`);
+    });
+
+    this.update.activated.subscribe((event)=>{
+      console.log(`event:`,event.previous);
+      console.log(`event:`,event.current);
+
+    });
+  }
 }
